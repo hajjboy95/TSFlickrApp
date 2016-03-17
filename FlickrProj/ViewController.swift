37393d70a1a24a9371322a84128b99656257abd3
hajjboy95 <gizmo-boy95@hotmail.com>
2016-03-17 13:13:10 +0000

    Initial Commit

---
+++ FlickrProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrProj
+//
+//  Created by Ismail El-habbash on 3/17/16.
+//  Copyright © 2016 Ismail El-Habbash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FlickrProjTests/FlickrProjTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlickrProjTests.swift
+//  FlickrProjTests
+//
+//  Created by Ismail El-habbash on 3/17/16.
+//  Copyright © 2016 Ismail El-Habbash. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrProj
+
+class FlickrProjTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

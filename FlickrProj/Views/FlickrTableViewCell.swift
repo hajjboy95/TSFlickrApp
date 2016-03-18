c379ad2b4e99291c69640ef48fadc46cb2a94dcf
hajjboy95 <gizmo-boy95@hotmail.com>
2016-03-18 11:02:25 +0000

    creates a uitableviewcell subsclass and adds it to storyboard

---
+++ FlickrProj/Views/FlickrTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FlickrTableViewCell.swift
+//  FlickrProj
+//
+//  Created by Ismail El-habbash on 3/18/16.
+//  Copyright © 2016 Ismail El-Habbash. All rights reserved.
+//
+
+import UIKit
+
+class FlickrTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

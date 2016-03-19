a6aa63ee1058216e7dbdcb8804cb01c2cf99bb60
hajjboy95 <gizmo-boy95@hotmail.com>
2016-03-19 12:23:27 +0000

    adds constants file to store ids and segue id

---
+++ FlickrProj/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FlickrProj
+//
+//  Created by Ismail El-habbash on 3/19/16.
+//  Copyright © 2016 Ismail El-Habbash. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let FlickrTableViewCellIdentifier    = "FlickrCell"
+    static let FlickrTableViewToDetailViewSegue = "ToDetail"
+    
+}
